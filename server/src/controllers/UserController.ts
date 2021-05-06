35fb95b201f807a56a8c4cbdade2e4bb23d1add6
mark.hulevsky <mark.hulevsky@dewmaker.online>
2021-05-05 18:39:18 -0700

    jwt access token implemented

---
+++ server/src/controllers/UserController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { UserModel } from "../models";
+import { IUser } from "../models/UserModel";
+
+class UserController {
+    getUserData = async (req: Request, res: Response) => {
+        try {
+            const id = req.decodedToken.id;
+            let document = await UserModel.findById(id);
+            if(!document) return res.status(400).json({ message: 'Пользователь не найден' });
+            return res.status(200).json(
+                {
+                    user: {
+                        
+                    }
+                }
+            )
+        } catch (error) {
+            return res.status(400).json({ message: 'Ошибка получения данных', details: { ...error } })
+        }
+    }
+}
+
+export default new UserController();
