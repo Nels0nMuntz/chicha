e335c56fd63d766afc42ddb83c6b54cdb443b6bc
arthur.lutsenko <arthur.lutsenko@dewmaker.online>
2021-05-18 10:17:17 -0700

    added transition to burger menu

--- client/src/components/HeaderSidebar/HeaderSidebar.tsx
+++ client/src/components/HeaderSidebar/HeaderSidebar.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import classnames from 'classnames';
+import { CSSTransition } from 'react-transition-group';
 
 import GroupIcon from '@material-ui/icons/Group';
 import PermContactCalendarIcon from '@material-ui/icons/PermContactCalendar';
@@ -8,30 +9,29 @@ import LiveHelpIcon from '@material-ui/icons/LiveHelp';
 import InfoIcon from '@material-ui/icons/Info';
 
 import style from './HeaderSidebar.module.scss';
+import './style.scss'
 
 
 const HeaderSidebar: React.FC = () => {
 
     const [drowerState, setDrowerState] = React.useState(false);
 
-    const ref = React.useRef((e: any) : void => {
-        if(e.target === btnRef.current) return;
+    const ref = React.useRef((e: any): void => {
+        if (e.target === btnRef.current) return;
         setDrowerState(false);
     });
 
     const btnRef = React.useRef(null);
 
     const onClickBurger = () => {
-        
-        console.log(drowerState);
-        if(!drowerState) {         
+        if (!drowerState) {
             setDrowerState(true);
             document.body.addEventListener('mousedown', ref.current);
-        }
-        if(drowerState){            
+        };
+        if (drowerState) {
             setDrowerState(false);
             document.body.removeEventListener('mousedown', ref.current);
-        }        
+        };
     };
 
     return (
@@ -51,30 +51,35 @@ const HeaderSidebar: React.FC = () => {
                 </div>
                 <p className={style.header_sidebarTitle}>Список диалогов</p>
             </div>
-            {drowerState && (
+            <CSSTransition
+                in={drowerState}
+                timeout={300}
+                unmountOnExit                
+                classNames='burger-menu'
+            >
                 <ul className={style.burgerMenu}>
                     <li className={style.burgerMenuItem}>
-                        <div className={style.itemIcon}><GroupIcon/></div>
+                        <div className={style.itemIcon}><GroupIcon /></div>
                         <div className={style.itemText}>New group</div>
                     </li>
                     <li className={style.burgerMenuItem}>
-                        <div className={style.itemIcon}><PermContactCalendarIcon/></div>
+                        <div className={style.itemIcon}><PermContactCalendarIcon /></div>
                         <div className={style.itemText}>Contacts</div>
                     </li>
                     <li className={style.burgerMenuItem}>
-                        <div className={style.itemIcon}><SettingsIcon/></div>
+                        <div className={style.itemIcon}><SettingsIcon /></div>
                         <div className={style.itemText}>Settings</div>
                     </li>
                     <li className={style.burgerMenuItem}>
-                        <div className={style.itemIcon}><LiveHelpIcon/></div>
+                        <div className={style.itemIcon}><LiveHelpIcon /></div>
                         <div className={style.itemText}>ChiCha FAQ</div>
                     </li>
                     <li className={style.burgerMenuItem}>
-                        <div className={style.itemIcon}><InfoIcon/></div>
+                        <div className={style.itemIcon}><InfoIcon /></div>
                         <div className={style.itemText}>About</div>
                     </li>
                 </ul>
-            )}
+            </CSSTransition>
         </div>
     )
 };
